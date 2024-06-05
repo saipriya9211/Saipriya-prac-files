2c1b27836c39aefe3398adbeb62d008fb876fe9d
saipriya9211 <168262989+saipriya9211@users.noreply.github.com>
2024-06-05 05:18:18 -0700

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,12 @@
+const obj = {
+    name : 'Sassy',
+    age : '18',
+    'fur color' : 'grey',
+    likes :['captain', 'milk'],
+    birthday :['month :7', 'date :7', 'year :1997']
+    
+
+    
+};
+const values = Object.values(obj);
+console.log(values);S
